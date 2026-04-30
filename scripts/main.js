90d8e9c473a933ac4747b91b5a9cbb8d82f145bc
SobczakL <36972429+SobczakL@users.noreply.github.com>
2026-04-29 21:16:46 -0400

    chore: testing fetch to backend basic api

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,72 @@
-
 const form = document.querySelector(".formSection_form")
+const retriever = document.querySelector(".tinyRetriever_button")
+const local = window.localStorage
 
-
-function formPressed(){
-    event.stopPropagation()
-    console.log("hello again mf")
-}
+local.setItem("test-tiny", "")
 
 function main(){
     console.log("hello")
 }
 main()
-form.addEventListener("submit", formPressed)
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const inputVal = document.getElementById("formInput").value
+    const output = document.querySelector(".linkSection_container-generatedLink")
+    let blender = 0
+
+    hashThis(inputVal)
+        .then((hex) => {
+            blender = hex
+            console.log(blender)
+            local.setItem("test-tiny", blender)
+            output.textContent = `Out: ${blender}`
+            }
+        )
+        .catch((err) => console.log(err))
+
+})
+
+retriever.addEventListener("click", () => {
+    let log = local.getItem("test-tiny")
+    // console.log(log)
+    request()
+    // unHashThis(log)
+})
+
+async function request(){
+    const url = "http://localhost:8080/hey"
+
+    try {
+        let response = await fetch(url)
+        if(!response.ok){
+            throw new error("errorrrrrr")
+        }
+
+        let result = await response.json()
+
+        if(result){
+            let data = JSON.parse(result)
+            console.log(data.message)
+        }
+    }
+    catch (error) {
+        console.error("error")
+    }
+
+}
+
+async function hashThis(link){
+    const endodedMsg = new TextEncoder().encode(link)
+
+    const hashHex = new Uint8Array(endodedMsg).toBase64()
+    return hashHex
+}
+
+async function unHashThis(hash){
+    const hashHex = Uint8Array.fromBase64(hash)
+    const decodeMsg = new TextDecoder().decode(hashHex)
+    console.log(decodeMsg)
+}
